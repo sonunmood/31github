0750e6c0c3ac0fdc7d684fa93a8ed6c2e69f958c
Sonun <usubalievasonun@gmail.com>
2023-11-22 21:18:10 +0600

    Grade Book

---
+++ CodeWars/main.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  CodeWars
+//
+//  Created by Sonun on 22/11/23.
+//
+
+import Foundation
+
+func getGrade(_ s1:Int, _ s2:Int, _ s3:Int) -> String {
+    
+    let avarageScore = (s1 + s2 + s3)/3
+    
+    switch avarageScore {
+    case 0...100: return "A"
+    case 80..<90: return "B"
+    case 70..<80: return "C"
+    case 60..<70: return "D"
+    case 0..<60:  return "F"
+    default:      return "error"
+    }
+    
+}
+
+var result = getGrade(80, 90, 30)
+print(result)
