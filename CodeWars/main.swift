1100c12e0881ec0082c275b072660ca20f459994
Sonun <usubalievasonun@gmail.com>
2023-12-10 22:55:32 +0600

    Calculate BMI

--- CodeWars/main.swift
+++ CodeWars/main.swift
@@ -395,3 +395,21 @@ func rentalCarCost(_ days: Int) -> Int {
 
 print(rentalCarCost(7))
 
+//Write function bmi that calculates body mass index (bmi = weight / height2).
+//if bmi <= 18.5 return "Underweight"
+//if bmi <= 25.0 return "Normal"
+//if bmi <= 30.0 return "Overweight"
+//if bmi > 30 return "Obese"
+
+func bmi(_ weight: Int, _ height: Double) -> String {
+    var bmi: Double = Double(weight) / (height * height)
+    
+    switch bmi {
+    case 0...18.5: return "Underweight"
+    case 18.6...25.0: return "Normal"
+    case 25.1...30.0: return "Overweight"
+    default: return "Obese"
+    }
+}
+
+print(bmi(57, 160.0))
