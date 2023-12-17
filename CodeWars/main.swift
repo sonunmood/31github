cbdf4a178faa6ee5e7998be0ae5ac83d8136570a
Sonun <usubalievasonun@gmail.com>
2023-12-17 19:47:19 +0600

    another variety of previous task

--- CodeWars/main.swift
+++ CodeWars/main.swift
@@ -579,3 +579,7 @@ func isDivisible(_ n: Int, _ x: Int, _ y: Int) -> Bool {
 }
 
 print(isDivisible(45, 5, 4))
+
+func isDivisible2(_ n: Int, _ x: Int, _ y: Int) -> Bool {
+    return n % x == 0 && n % y == 0
+}
